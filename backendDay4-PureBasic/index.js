d1ddd192192576dbddbd2587d5d94838e12a5172
Sahul54 <ksahul9576@gmail.com>
2024-05-30 18:58:08 +0530

    day 4 pure basic

---
+++ backendDay4-PureBasic/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+    // console.log("App start");
+    res.send(`<h1> Hello jii</h1>`)
+});
+
+app.post("/home", (req, res) => {
+     res.send("Recived Post");
+});
+
+app.delete("/delete", (req, res) => {
+    res.send("Delete msg")
+});
+
+app.listen(port, ()=>{
+    console.log("App Start");
+})
