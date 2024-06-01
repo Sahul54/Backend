4eb06c0dcc25ac73077269a28c76c1b4ee21677e
Sahul54 <ksahul9576@gmail.com>
2024-06-01 21:13:28 +0530

    signup done and move to login

---
+++ backendDay6-AuthApp/config/database.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require("mongoose");
+
+require("dotenv").config();
+
+exports.connect = () =>{
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>{
+        console.log("DB connected Successfuly!");
+    })
+    .catch((err) =>{
+        console.log("DB connecton issue");
+        console.error(err);
+        process.exit(1);
+    })
+}
--- backendDay6-AuthApp/index.js
+++ backendDay6-AuthApp/index.js
@@ -1,7 +1,18 @@
-const express = rerquire("express");
+const express = require("express");
 const app = express();
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+require("./config/database").connect();
+
+// route import and mount
+const user = require("./routes/user");
+app.use("/api/v1", user)
+
+// activate
+app.listen(PORT, () =>{
+    console.log(`App started ${PORT}`);
+})
